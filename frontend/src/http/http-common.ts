5847c991530c7cf51f85a2bf26af9531bd5caaa3
owenyi <67588446+owenyi@users.noreply.github.com>
2021-08-09 14:27:41 +0900

    test

--- frontend/src/http/http-common.ts
+++ frontend/src/http/http-common.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://127.0.0.1:8080/api",
+    baseURL: "http://127.0.0.1:80/api",
     headers: {
         'Access-Control-Allow-Origin': '*',
         'Access-Control-Allow-Headers': '*',
