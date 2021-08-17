5e0f23c0e2ee1c10919e128235860dad770e1f6a
owenyi <67588446+owenyi@users.noreply.github.com>
2021-08-17 18:14:34 +0900

    test

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,7 +1,6 @@
 module.exports = {
   devServer: {
-    proxy: 'http://127.0.0.1:80/api',
-    // disableHostCheck: true,
+    disableHostCheck: true,
       // proxy: {
       //     '/': {
       //         "target": 'http://127.0.0.1:8080/board',
